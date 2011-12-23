d5e40fd97ad783c207d4b979a6304d686d1c3f76
Scott A Garman <sag5066@gmail.com>
2011-12-23 13:35:41 -0800

    changing sdk target.

---
+++ src/com/scottagarman/android/xblAvatar/activities/actionbar/ABLauncherActivity.java
@@ -0,0 +1,47 @@
+package com.scottagarman.android.xblAvatar.activities.actionbar;
+
+import android.R;
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import com.scottagarman.android.xblAvatar.TabListener;
+import com.scottagarman.android.xblAvatar.activities.fragments.HomeFragment;
+
+public class ABLauncherActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+            // Notice that setContentView() is not used, because we use the root
+            // android.R.id.content as the container for each fragment
+
+            // setup action bar for tabs
+            ActionBar actionBar = getActionBar();
+            actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+            actionBar.setDisplayShowTitleEnabled(false);
+            actionBar.setDisplayShowHomeEnabled(false);
+
+            ActionBar.Tab tab = actionBar.newTab()
+                    .setIcon(R.drawable.ic_dialog_email)
+                    .setTabListener(new TabListener<HomeFragment>(this, "first", HomeFragment.class));
+            actionBar.addTab(tab);
+
+            tab = actionBar.newTab()
+                .setIcon(R.drawable.ic_dialog_dialer)
+                .setTabListener(new TabListener<HomeFragment>(this, "second", HomeFragment.class));
+            actionBar.addTab(tab);
+
+            // check for last open tab
+            if (savedInstanceState != null) {
+                actionBar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));
+            }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        // save current selected tab
+        outState.putInt("tab", getActionBar().getSelectedNavigationIndex());
+    }
+}
---
+++ src/com/scottagarman/android/xblAvatar/activities/fragments/HomeFragment.java
@@ -0,0 +1,22 @@
+package com.scottagarman.android.xblAvatar.activities.fragments;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.scottagarman.android.xblAvatar.R;
+
+public class HomeFragment extends Fragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home, container, true);
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
