62f0a7cb588b9c71d7adcadf8433ed56bf0bb2b6
Scott A Garman <sag5066@gmail.com>
2011-12-23 19:36:54 -0800

    Adding scrolling now.

--- src/com/scottagarman/android/xblAvatar/activities/actionbar/ABLauncherActivity.java
+++ src/com/scottagarman/android/xblAvatar/activities/actionbar/ABLauncherActivity.java
@@ -1,47 +1,45 @@
 package com.scottagarman.android.xblAvatar.activities.actionbar;
 
-import android.R;
 import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
-import com.scottagarman.android.xblAvatar.TabListener;
+import android.support.v4.view.ViewPager;
 import com.scottagarman.android.xblAvatar.activities.fragments.AboutFragment;
 import com.scottagarman.android.xblAvatar.activities.fragments.FriendsListFragment;
 import com.scottagarman.android.xblAvatar.activities.fragments.HomeFragment;
+import com.scottagarman.android.xblAvatar.adapters.TabsAdapter;
 
 public class ABLauncherActivity extends Activity {
+
+    private ViewPager mViewPager;
+    private TabsAdapter mTabsAdapter;
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
 
-            // Notice that setContentView() is not used, because we use the root
-            // android.R.id.content as the container for each fragment
+        mViewPager = new ViewPager(this);
+        mViewPager.setId(1234);
+        setContentView(mViewPager);
 
-            // setup action bar for tabs
-            ActionBar actionBar = getActionBar();
-            actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-            actionBar.setDisplayShowTitleEnabled(false);
-            actionBar.setDisplayShowHomeEnabled(false);
+        // Notice that setContentView() is not used, because we use the root
+        // android.R.id.content as the container for each fragment
 
-            ActionBar.Tab tab = actionBar.newTab()
-                    .setIcon(android.R.drawable.ic_dialog_email)
-                    .setTabListener(new TabListener<HomeFragment>(this, "first", HomeFragment.class));
-            actionBar.addTab(tab);
+        // setup action bar for tabs
+        ActionBar actionBar = getActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setDisplayShowHomeEnabled(false);
 
-            tab = actionBar.newTab()
-                .setIcon(android.R.drawable.ic_dialog_dialer)
-                .setTabListener(new TabListener<FriendsListFragment>(this, "second", FriendsListFragment.class));
-            actionBar.addTab(tab);
+        mTabsAdapter = new TabsAdapter(this, mViewPager);
+        mTabsAdapter.addTab(actionBar.newTab().setText("home"), HomeFragment.class, null);
+        mTabsAdapter.addTab(actionBar.newTab().setText("friends"), FriendsListFragment.class, null);
+        mTabsAdapter.addTab(actionBar.newTab().setText("about"), AboutFragment.class, null);
 
-            tab = actionBar.newTab()
-                .setIcon(R.drawable.ic_dialog_alert)
-                .setTabListener(new TabListener<AboutFragment>(this, "third", AboutFragment.class));
-            actionBar.addTab(tab);
-
-            // check for last open tab
-            if (savedInstanceState != null) {
-                actionBar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));
-            }
+        // check for last open tab
+        if (savedInstanceState != null) {
+            actionBar.setSelectedNavigationItem(savedInstanceState.getInt("tab", 0));
+        }
     }
 
     @Override
