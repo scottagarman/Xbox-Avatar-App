720cf9fa30c782ccf1b7d7bf3d49a1e11f33f3a7
Scott A Garman <sag5066@gmail.com>
2011-12-24 17:05:43 -0800

    Adding avatar mgr to fragment.

--- src/com/scottagarman/android/xblAvatar/activities/fragments/HomeFragment.java
+++ src/com/scottagarman/android/xblAvatar/activities/fragments/HomeFragment.java
@@ -5,17 +5,30 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
 import com.scottagarman.android.xblAvatar.R;
+import com.scottagarman.android.xblAvatar.managers.AvatarsManager;
 
 public class HomeFragment extends Fragment {
 
+    private EditText mEditText;
+    private AvatarsManager mAvatarManager;
+    
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        mAvatarManager = new AvatarsManager(getActivity());
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        ViewGroup v = (ViewGroup)inflater.inflate(R.layout.fragment_home, container, false);
+        mEditText = (EditText)v.findViewById(R.id.home_edit);
+        return v;
+    }
+    
+    public void onAddTouched(View v) {
+
     }
 }
